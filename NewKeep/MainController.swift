a149fb3d87c31822c5bdeed72c24099d35b7ec74
Eyal Avisar <enfer0666@yahoo.com>
2020-10-06 21:58:02 +0300

    costume layout works

--- NewKeep/MainController.swift
+++ NewKeep/MainController.swift
@@ -8,6 +8,18 @@
 
 import UIKit
 
+extension MainController: NotesLayoutDelegate {
+    func collectionView(_ collectionView: UICollectionView, heightForItemAt indexPath: IndexPath, with width: CGFloat) -> CGFloat {
+        return calculateLabelSize(indexPath: indexPath)
+    }
+    
+  func collectionView(
+      _ collectionView: UICollectionView,
+      heightForLabelAtIndexPath indexPath:IndexPath) -> CGFloat {
+    return calculateLabelSize(indexPath: indexPath)
+  }
+}
+
 class MainController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     
@@ -20,11 +32,26 @@ class MainController: UIViewController, UICollectionViewDelegate, UICollectionVi
         super.viewDidLoad()
         notesCollection.delegate = self
         notesCollection.dataSource = self
+        
+        if let layout = notesCollection?.collectionViewLayout as? NotesLayout {
+          layout.delegate = self
+        }
+
+        notesCollection?.backgroundColor = .clear
+        notesCollection?.contentInset = UIEdgeInsets(top: 23, left: 5, bottom: 10, right: 5)
     }
 
     override func viewDidAppear(_ animated: Bool) {
+        print("items in collection view")
+        print(notesCollection.numberOfItems(inSection: 0))
         notesCollection.reloadData()
+        
     }
+    
+//    override func viewDidLayoutSubviews() {
+//        super.viewDidLayoutSubviews()
+//        notesCollection.collectionViewLayout.invalidateLayout()
+//    }
     
     //MARK: collectionView datasource + delegate + flowlayout
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -50,21 +77,23 @@ class MainController: UIViewController, UICollectionViewDelegate, UICollectionVi
         writeNote(createNoteButton)
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        var text = "\(notes[indexPath.row].title!)\n"
-        text += "\(notes[indexPath.row].content!)"
-        
-        let label = UILabel()
-        label.text = text
-        label.numberOfLines = 0
-        label.textAlignment = .left
-        label.font = UIFont.systemFont(ofSize: 20)
-        var height = max(label.intrinsicContentSize.height * 2, 70)
-        height = min(height, view.frame.height / 2.0)
-    
-        return CGSize(width: view.frame.width / 2 - 5 , height: height + 5)
-    }
+//        var text = "\(notes[indexPath.row].title!)\n"
+//        text += "\(notes[indexPath.row].content!)"
+//
+//        let label = UILabel()
+//        label.text = text
+//        label.numberOfLines = 0
+//        label.textAlignment = .left
+//        label.font = UIFont.systemFont(ofSize: 20)
+//        var height = max(label.intrinsicContentSize.height * 2, 70)
+//        height = min(height, view.frame.height / 2.0)
+//
+//        return CGSize(width: view.frame.width / 2 - 5 , height: height + 5)
+//        let itemSize = (collectionView.frame.width - (collectionView.contentInset.left + collectionView.contentInset.right + 10)) / 2
+//        return CGSize(width: itemSize, height: itemSize)
+//    }
     
     //MARK: IBAction
     @IBAction func writeNote(_ sender: UIButton) {
@@ -75,4 +104,22 @@ class MainController: UIViewController, UICollectionViewDelegate, UICollectionVi
         navigationController?.pushViewController(noteMaker, animated: true)
     }
     
+    
+    //MARK: helper functions
+    
+    func calculateLabelSize(indexPath:IndexPath) -> CGFloat {
+        var text = "\(notes[indexPath.row].title!)\n"
+        text += "\(notes[indexPath.row].content!)"
+        
+        let label = UILabel()
+        label.text = text
+        label.numberOfLines = 0
+        label.textAlignment = .left
+        label.font = UIFont.systemFont(ofSize: 20)
+        var labelHeight = max(label.intrinsicContentSize.height * 2, 70)
+        labelHeight = min(labelHeight, view.frame.height / 2.0)
+        
+        print("height calculated: \(labelHeight)")
+        return labelHeight
+    }
 }
---
+++ NewKeep/NotesLayout.swift
@@ -0,0 +1,99 @@
+//
+//  NotesLayout.swift
+//  NewKeep
+//
+//  Created by eyal avisar on 06/10/2020.
+//  Copyright © 2020 eyal avisar. All rights reserved.
+//
+
+import UIKit
+
+protocol NotesLayoutDelegate: AnyObject {
+  func collectionView(
+    _ collectionView: UICollectionView,
+    heightForLabelAtIndexPath indexPath: IndexPath) -> CGFloat
+}
+
+class NotesLayout: UICollectionViewLayout {
+    
+    weak var delegate: NotesLayoutDelegate?
+
+    private let numberOfColumns = 2
+    private let cellPadding: CGFloat = 6
+
+    private var cache: [UICollectionViewLayoutAttributes] = []
+
+    private var contentHeight: CGFloat = 0
+
+    private var contentWidth: CGFloat {
+      guard let collectionView = collectionView else {
+        return 0
+      }
+      let insets = collectionView.contentInset
+        return collectionView.bounds.width - (insets.left + insets.right)
+    }
+
+    override var collectionViewContentSize: CGSize {
+      return CGSize(width: contentWidth, height: contentHeight)
+    }
+    
+    override func prepare() {
+      guard
+//        cache.isEmpty,
+        let collectionView = collectionView
+        else {
+          return
+      }
+
+    let columnWidth = contentWidth / CGFloat(numberOfColumns)
+      var xOffset: [CGFloat] = []
+      for column in 0..<numberOfColumns {
+        xOffset.append(CGFloat(column) * columnWidth)
+      }
+      var column = 0
+      var yOffset: [CGFloat] = .init(repeating: 0, count: numberOfColumns)
+        
+      for item in 0..<collectionView.numberOfItems(inSection: 0) {
+        let indexPath = IndexPath(item: item, section: 0)
+          
+        let labelHeight = delegate?.collectionView(
+          collectionView,
+          heightForLabelAtIndexPath: indexPath) ?? 70
+        let height = cellPadding * 2 + labelHeight
+        let frame = CGRect(x: xOffset[column],
+                           y: yOffset[column],
+                           width: columnWidth,
+                           height: height)
+        let insetFrame = frame.insetBy(dx: cellPadding, dy: cellPadding)
+          
+        let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath)
+        attributes.frame = insetFrame
+        cache.append(attributes)
+          
+        contentHeight = max(contentHeight, frame.maxY)
+        yOffset[column] = yOffset[column] + height
+        
+//        print(yOffset)
+        
+        column = column < (numberOfColumns - 1) ? (column + 1) : 0
+      }
+    }
+
+    override func layoutAttributesForElements(in rect: CGRect)
+        -> [UICollectionViewLayoutAttributes]? {
+      var visibleLayoutAttributes: [UICollectionViewLayoutAttributes] = []
+      
+      for attributes in cache {
+        if attributes.frame.intersects(rect) {
+          visibleLayoutAttributes.append(attributes)
+        }
+      }
+      return visibleLayoutAttributes
+    }
+
+    override func layoutAttributesForItem(at indexPath: IndexPath)
+        -> UICollectionViewLayoutAttributes? {
+      return cache[indexPath.item]
+    }
+
+}
